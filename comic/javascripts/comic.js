217aff2e1987e47100b8cc413eb80746f7b8d250
Preeti Viswanathan <vis.preeti@gmail.com>
2016-03-28 15:44:08 -0700

    Add link to restart animations and refactor code

--- comic/javascripts/comic.js
+++ comic/javascripts/comic.js
@@ -3,11 +3,21 @@ $(function() {
       delay = 1500,
       speed = 250;
 
-  $blinds.each(function(i) {
-    var $blind = $blinds.eq(i);
-    $blind.delay(delay * i + speed).animate({
-      top: "+=" + $blind.height(),
-      height: 0
-    }, 250);
+  function startAnimation() {
+    $blinds.each(function(i) {
+      var $blind = $blinds.eq(i);
+      $blind.delay(delay * i + speed).animate({
+        top: "+=" + $blind.height(),
+        height: 0
+      }, speed);
+    });
+  }
+
+  $("a").on("click", function(e) {
+    e.preventDefault();
+    $blinds.finish();
+    $blinds.removeAttr("style");
+    startAnimation();
   });
+  startAnimation();
 });
